47a723f1b8e36514dfd2c0a36da76bfc640de8d3
Bryan J Kariuki <webdeveloperjoekariuki@gmail.com>
2025-11-03 14:34:16 -0800

    commit

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,7 +3,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query all books by a specific author
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    books = author.books.all()
+    books = Book.objects.filter(author=author)
     return books
 
 
@@ -17,12 +17,6 @@ def get_books_in_library(library_name):
 # Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    librarian = library.librarian
+    return librarian
 
-
-# Example usage (run in Django shell)
-if __name__ == "__main__":
-    # Example calls (ensure you’ve created objects via Django shell or admin)
-    print(get_books_by_author("J.K. Rowling"))
-    print(get_books_in_library("Central Library"))
-    print(get_librarian_for_library("Central Library"))
