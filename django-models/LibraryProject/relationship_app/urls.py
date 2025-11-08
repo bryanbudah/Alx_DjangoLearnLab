e18d4fe3ea85dd0ce0d5dc848a154b0d471a1f5c
Bryan J Kariuki <webdeveloperjoekariuki@gmail.com>
2025-11-08 03:41:10 -0800

    detail

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
 from .views import CustomLoginView, CustomLogoutView, register
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views 
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
