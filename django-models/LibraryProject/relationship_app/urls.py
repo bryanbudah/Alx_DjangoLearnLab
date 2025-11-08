307a1261868f0b77e32618198db911da2d044cb3
Bryan J Kariuki <webdeveloperjoekariuki@gmail.com>
2025-11-08 05:16:42 -0800

    line

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,10 +9,10 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
-    # Book CRUD URLs with permission enforcement
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
-    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
+    # Book CRUD URLs (ALX expects no "books/" prefix)
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 
     # ------------------------
     # Authentication URLs
