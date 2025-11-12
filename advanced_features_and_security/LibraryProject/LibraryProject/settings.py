16b9fbe29990148dc1eead5a8f699f9a6f9ae817
Bryan J Kariuki <webdeveloperjoekariuki@gmail.com>
2025-11-12 12:52:37 -0800

    ball

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -12,27 +12,23 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-# ----------------------------
-# Base directory
-# ----------------------------
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# ----------------------------
-# Security settings
-# ----------------------------
-
 SECRET_KEY = 'django-insecure-#=8#y3bmorxat-*a*t8p#px%=8q-u8n5gx_%7n&l399@kg^dp6'
+
 DEBUG = False
+
 ALLOWED_HOSTS = ['*']
 
+# ----------------------------
 # Security best practices
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_HSTS_SECONDS = 31536000  # 1 year
+# ----------------------------
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
-SECURE_SSL_REDIRECT = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 X_FRAME_OPTIONS = 'DENY'
@@ -126,4 +122,3 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # ----------------------------
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
